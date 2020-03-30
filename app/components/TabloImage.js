f571abdb1b1f9325706551d265ed9630bf3f5080
jesse <jessedp@gmail.com>
2020-03-30 18:25:09 -0400

    fix maxheight

--- app/components/TabloImage.js
+++ app/components/TabloImage.js
@@ -16,7 +16,7 @@ export default class TabloImage extends Component<Props> {
     const host = Api.device.private_ip;
     const style = {};
     if (maxHeight && maxHeight > 50) {
-      style.maxHeight = maxHeight;
+      style.maxHeight = `${maxHeight}px`;
     }
     let url = unknownImg;
     if (imageId && parseInt(imageId, 10)) {
