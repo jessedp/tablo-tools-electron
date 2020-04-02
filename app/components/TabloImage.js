40000cc388d63069e77c7c102627338915aaadd6
jesse <jessedp@gmail.com>
2020-04-02 01:16:04 -0400

    make that a gte

--- app/components/TabloImage.js
+++ app/components/TabloImage.js
@@ -17,7 +17,7 @@ export default class TabloImage extends Component<Props> {
     const { imageId, maxHeight } = this.props;
     const host = Api.device.private_ip;
     const style = {};
-    if (maxHeight && maxHeight > 50) {
+    if (maxHeight && maxHeight >= 50) {
       style.maxHeight = `${maxHeight}px`;
     }
     let url = unknownImg;
