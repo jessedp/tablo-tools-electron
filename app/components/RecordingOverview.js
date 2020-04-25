2218541cf609ed24fc77f4201a3e4094a08ffbf4
jesse <jessedp@gmail.com>
2020-04-25 00:48:45 -0400

    make program type work (manual recording)

--- app/components/RecordingOverview.js
+++ app/components/RecordingOverview.js
@@ -27,7 +27,7 @@ export default class RecordingOverview extends Component<Props> {
                 <td>{airing.episodeNum}</td>
               </>
             ) : (
-              ''
+              <></>
             )}
             <th>Watched</th>
             <td>{boolStr(airing.userInfo.watched)}</td>
