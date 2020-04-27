656ab352c514bea32cbfd3c2b8353dad694b55fe
jesse <jessedp@gmail.com>
2020-04-27 19:12:20 -0400

    add beforeSend for sentry to fix sourcemaps!

--- app/sentry.js
+++ app/sentry.js
@@ -11,6 +11,31 @@ if (process.env.NODE_ENV === 'production') {
       'https://a19dbdc56dc54776a48d2acce4c99ddc@o381395.ingest.sentry.io/5208692',
     org: 'jessedp',
     project: 'tablo-tools-electron',
-    release: version
+    release: version,
+    beforeSend(event) {
+      if (
+        !event.exception ||
+        !event.exception.values ||
+        !event.exception.values[0]
+      ) {
+        return event;
+      }
+
+      const value = event.exception.values[0];
+
+      if (value.stacktrace && value.stacktrace.frames) {
+        value.stacktrace.frames.forEach(frame => {
+          if (frame.filename.includes('dist/')) {
+            // this is stupid, but it seems to work for now @sentry/electron": "1.3.0
+            // eslint-disable-next-line no-param-reassign
+            frame.filename = frame.filename.replace('dist/', 'app/dist/');
+            // eslint-disable-next-line no-param-reassign
+            frame.filename = frame.filename.replace('app/app/', 'app/');
+          }
+        });
+      }
+
+      return event;
+    }
   });
 }
