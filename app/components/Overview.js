615bf418d3c470a7b2908e24b71af39b7c2af966
jesse <jessedp@gmail.com>
2020-04-29 10:52:27 -0400

    change top bar, add new stats components

--- app/components/Overview.js
+++ app/components/Overview.js
@@ -1,19 +1,21 @@
 // @flow
 import React, { Component } from 'react';
+import PubSub from 'pubsub-js';
+
 import compareVersions from 'compare-versions';
 
-import Container from 'react-bootstrap/Container';
 import Alert from 'react-bootstrap/Alert';
 
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
-import DbInfoTable from './DbInfoTable';
-import ServerInfoTable from './ServerInfoTable';
+import DbStats from './DbStats';
 
-import { recDbCreated } from '../utils/db';
-import RelativeDate from './RelativeDate';
 import ComskipDetails from './ComskipDetails';
+import TimeStats from './TimeStats';
+import ResolutionStats from './ResolutionStats';
+import { parseSeconds, readableBytes } from '../utils/utils';
+import ChannelStats from './ChannelStats';
 
 const Store = require('electron-store');
 
@@ -21,7 +23,9 @@ const store = new Store();
 
 type Props = {};
 type State = {
-  currentDevice: Object
+  currentDevice: Object,
+  duration: Array<number>,
+  size: number
 };
 
 export default class Overview extends Component<Props, State> {
@@ -30,17 +34,40 @@ export default class Overview extends Component<Props, State> {
   constructor() {
     super();
     const currentDevice = store.get('CurrentDevice');
-    this.state = { currentDevice };
+    this.state = { currentDevice, duration: [], size: 0 };
+
+    (this: any).refresh = this.refresh.bind(this);
+  }
+
+  async componentDidMount() {
+    this.refresh();
+    this.psToken = PubSub.subscribe('DB_CHANGE', this.refresh);
+  }
+
+  componentWillUnmount(): * {
+    PubSub.unsubscribe(this.psToken);
+  }
+
+  psToken = null;
+
+  async refresh() {
+    const { RecDb } = global;
+
+    const recs = await RecDb.asyncFind({});
+    const duration = recs.reduce(
+      (a, b) => a + (b.video_details.duration || 0),
+      0
+    );
+    const size = recs.reduce((a, b) => a + (b.video_details.size || 0), 0);
+
+    this.setState({ duration: parseSeconds(duration), size });
   }
 
   render() {
-    const { currentDevice } = this.state;
-    let checked = '';
+    const { currentDevice, duration, size } = this.state;
 
     if (!currentDevice)
       return <Alert variant="warning">No device selected</Alert>;
-
-    checked = new Date(currentDevice.inserted);
 
     let comskipAvailable = false;
     if (currentDevice.server_version) {
@@ -49,40 +76,61 @@ export default class Overview extends Component<Props, State> {
     }
 
     return (
-      <Container>
+      <div className="section">
         <Row className="p-2 m-2 border bg-light">
           <Col>
-            Last Device Found: <RelativeDate date={checked} />
+            Recording Time: &nbsp;
+            {duration[0]} months {duration[1]} days {duration[2]} weeks{' '}
+            {duration[3]} minutes
           </Col>
           <Col>
-            DB Built: <RelativeDate date={recDbCreated()} />
+            Recording Size: &nbsp;
+            {readableBytes(size)}
           </Col>
         </Row>
-        <Row>
-          <Col>
-            <Alert variant="primary" className="p-2 m-0">
-              Recordings
-            </Alert>
-            <DbInfoTable />
-          </Col>
-          {comskipAvailable ? (
+
+        <div className="scrollable-area">
+          <Row>
             <Col>
               <Alert variant="primary" className="p-2 m-0">
-                Commercial Skip Stats
+                Recordings
               </Alert>
-              <ComskipDetails />
+              <DbStats />
             </Col>
-          ) : (
-            ''
-          )}
-          <Col>
-            <Alert variant="primary" className="p-2 m-0">
-              Current Tablo
-            </Alert>
-            <ServerInfoTable />
-          </Col>
-        </Row>
-      </Container>
+
+            <Col>
+              <Alert variant="primary" className="p-2 m-0">
+                Resolution/Channel Stats
+              </Alert>
+              <ResolutionStats />
+            </Col>
+
+            <Col>
+              <Alert variant="primary" className="p-2 m-0">
+                Channel/Show Stats
+              </Alert>
+              <ChannelStats />
+            </Col>
+
+            {comskipAvailable ? (
+              <Col>
+                <Alert variant="primary" className="p-2 m-0">
+                  Commercial Skip Stats
+                </Alert>
+                <ComskipDetails />
+              </Col>
+            ) : (
+              ''
+            )}
+            <Col>
+              <Alert variant="primary" className="p-2 m-0">
+                Time Stats
+              </Alert>
+              <TimeStats />
+            </Col>
+          </Row>
+        </div>
+      </div>
     );
   }
 }
