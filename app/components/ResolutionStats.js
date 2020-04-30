d080bd18d967370f379b411060f4f9bc2cd64c15
jesse <jessedp@gmail.com>
2020-04-29 20:51:29 -0400

    allow drilling into channel

--- app/components/ResolutionStats.js
+++ app/components/ResolutionStats.js
@@ -12,6 +12,7 @@ type Props = {};
 
 type State = {
   recTotal: number,
+  selResolution: string,
   resolutionData: Array<Object>,
   resolutionKeys: Array<string>
 };
@@ -25,10 +26,14 @@ export default class ResolutionStats extends Component<Props, State> {
     super(props);
     this.state = {
       recTotal: 0,
+      selResolution: '',
       resolutionData: [],
       resolutionKeys: []
     };
+
     (this: any).refresh = this.refresh.bind(this);
+    (this: any).chartClick = this.chartClick.bind(this);
+    (this: any).clearResolution = this.clearResolution.bind(this);
   }
 
   async componentDidMount() {
@@ -42,41 +47,63 @@ export default class ResolutionStats extends Component<Props, State> {
 
   async refresh() {
     const { RecDb } = global;
+    const { selResolution } = this.state;
     const recTotal = await RecDb.asyncCount({});
 
     /** resolution * */
     const recs = await RecDb.asyncFind({});
     const resCounts = {};
-
-    recs.forEach(rec => {
-      const { channel } = rec.airing_details.channel;
-      const { resolution } = channel;
-      const title = `${channel.network} (${channel.call_sign})`;
-      if (!resCounts[resolution]) resCounts[resolution] = {};
-      resCounts[resolution][title] = resCounts[resolution][title]
-        ? resCounts[resolution][title] + 1
-        : 1;
-    });
-
-    const resolutionData = [];
-    const resolutionKeys = [];
     const resMap = {
       hd_1080: 'HD 1080',
       hd_720: 'HD 720',
       sd: 'SD'
     };
+
+    const counter = [];
+    recs.forEach(rec => {
+      const { channel } = rec.airing_details.channel;
+      const { resolution } = channel;
+
+      const title = channel.network;
+
+      if (selResolution === resMap[resolution]) {
+        console.log(
+          'selResolution === resolution',
+          selResolution,
+          resMap[resolution]
+        );
+        resCounts[title] = resCounts[title] ? resCounts[title] + 1 : 1;
+        console.log(resCounts[title]);
+      } else if (!selResolution) {
+        const key = `${title}`;
+        if (!counter.includes(key)) {
+          counter.push(key);
+          resCounts[resolution] = resCounts[resolution]
+            ? resCounts[resolution] + 1
+            : 1;
+        }
+      }
+    });
+    console.log('resCounts', resCounts);
+
+    const resolutionData = [];
+    const resolutionKeys = [];
     Object.keys(resCounts).forEach(key => {
-      const resolution = {};
-      Object.keys(resCounts[key]).forEach(title => {
-        resolution[title] = resCounts[key][title];
-        resolutionKeys.push(title);
-      });
-      resolution.resolution = resMap[key] ? resMap[key] : '??';
-      resolutionData.push(resolution);
+      if (!selResolution) {
+        resolutionData.push({
+          resolution: resMap[key],
+          resolutions: resCounts[key]
+        });
+        resolutionKeys.push('resolutions');
+      } else {
+        resolutionData.push({ resolution: key, channels: resCounts[key] });
+        resolutionKeys.push('channels');
+      }
     });
 
     resolutionData.sort((a, b) => (a.resolution > b.resolution ? 1 : -1));
 
+    console.log(resolutionData);
     this.setState({
       recTotal,
       resolutionData,
@@ -84,8 +111,25 @@ export default class ResolutionStats extends Component<Props, State> {
     });
   }
 
+  chartClick = async (data: Object) => {
+    if (data.indexValue) {
+      await this.setState({ selResolution: data.indexValue });
+      this.refresh();
+    }
+  };
+
+  clearResolution = async () => {
+    await this.setState({ selResolution: '' });
+    this.refresh();
+  };
+
   render() {
-    const { recTotal, resolutionData, resolutionKeys } = this.state;
+    const {
+      recTotal,
+      selResolution,
+      resolutionData,
+      resolutionKeys
+    } = this.state;
     if (!recTotal)
       return (
         <Alert variant="light" className="p-2 m-0">
@@ -100,6 +144,8 @@ export default class ResolutionStats extends Component<Props, State> {
           indexBy="resolution"
           keys={resolutionKeys}
           scheme="pastel2"
+          onClick={!selResolution ? this.chartClick : () => {}}
+          back={selResolution ? this.clearResolution : null}
         />
         <ResolutionModal resolutionData={resolutionData} />
       </Col>
