5b1ca51ec3073efd4f0765a130a0eeff6ac6a303
jesse <jessedp@gmail.com>
2020-05-06 13:19:32 -0400

    make MatchesToBadges reusable. I bet I rename one of these

---
+++ app/components/SearchFilterMatches.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Badge from 'react-bootstrap/Badge';
+
+type Props = {
+  matches: Array<Object>,
+  prefix: string,
+  className: string
+};
+
+export default function MatchesToBadges(props: Props) {
+  const { matches, prefix } = props;
+  let { className } = props;
+  if (!className) className = 'badge-med';
+
+  className = `${className} ml-2 p-1`;
+
+  const newPrefix = prefix || `${Math.random() * 999999}`;
+
+  return matches.map(item => {
+    const key = `${newPrefix}-${Math.random() * 99999999999999}`;
+    return (
+      <Badge pill className={className} key={key} variant="dark">
+        <span className="p-1 m-0">{item.text}</span>
+      </Badge>
+    );
+  });
+}
