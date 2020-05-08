17f16d38d6eefd5a8d9785b0c54acabe14133977
jesse <jessedp@gmail.com>
2020-05-08 00:18:00 -0400

    make the last route/page sticky. bet they make me make this an option

--- app/Routes.js
+++ app/Routes.js
@@ -1,5 +1,7 @@
-import React from 'react';
-import { Switch, Route } from 'react-router';
+import React, { useEffect } from 'react';
+import { Switch, Route, Redirect } from 'react-router';
+import { useHistory } from 'react-router-dom';
+
 import routes from './constants/routes.json';
 import App from './containers/App';
 import HomePage from './containers/HomePage';
@@ -10,18 +12,34 @@ import Shows from './components/Shows';
 import Search from './components/Search';
 import Settings from './components/Settings';
 
-export default () => (
-  <App>
-    <Switch>
-      <Route path={routes.OVERVIEW} component={OverviewPage} />
-      <Route path={routes.COUNTER} component={CounterPage} />
-      <Route path={routes.BUILD} component={Build} />
-      <Route path={routes.SHOWS} component={Shows} />
-      <Route path={routes.SEARCH} component={Search} />
-      <Route path={routes.SETTINGS} component={Settings} />
+export default () => {
+  const history = useHistory();
 
-      {/* Put anything not HOME above this!! */}
-      <Route path={routes.HOME} component={HomePage} />
-    </Switch>
-  </App>
-);
+  useEffect(() => {
+    return history.listen(location => {
+      localStorage.setItem('lastPath', location.pathname);
+    });
+  }, [history]);
+
+  let lastPath = localStorage.getItem('lastPath');
+  if (!lastPath) {
+    lastPath = routes.HOME;
+  }
+
+  return (
+    <App>
+      <Switch>
+        <Redirect exact from="/" to={lastPath} />
+        <Route path={routes.OVERVIEW} component={OverviewPage} />
+        <Route path={routes.COUNTER} component={CounterPage} />
+        <Route path={routes.BUILD} component={Build} />
+        <Route path={routes.SHOWS} component={Shows} />
+        <Route path={routes.SEARCH} component={Search} />
+        <Route path={routes.SETTINGS} component={Settings} />
+
+        {/* Put anything not HOME above this!! */}
+        <Route path={routes.HOME} component={HomePage} />
+      </Switch>
+    </App>
+  );
+};
