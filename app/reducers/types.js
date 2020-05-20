1fc3e46af0480fe0f5bd9227e9d8df29ec7b8462
jesse <jessedp@gmail.com>
2020-05-20 00:25:00 -0400

    split type defs around

--- app/reducers/types.js
+++ app/reducers/types.js
@@ -1,28 +1,23 @@
 import type { Dispatch as ReduxDispatch, Store as ReduxStore } from 'redux';
 
+import Airing from '../utils/Airing';
+
 export type counterStateType = {
   +counter: number
+};
+
+export type actionListType = {
+  +airings: Array<Airing>
 };
 
 export type Action = {
   +type: string
 };
 
+export type GetActionList = () => actionListType;
+
 export type GetState = () => counterStateType;
 
 export type Dispatch = ReduxDispatch<Action>;
 
 export type Store = ReduxStore<GetState, Action>;
-
-export type TabloImage = {
-  image_id: number,
-  has_title: boolean
-};
-
-export type ShowCounts = {
-  airing_count: number,
-  unwatched_count: number,
-  protected_count: number,
-  watched_and_protected_count: number,
-  failed_count: number
-};
---
+++ app/utils/types.js
@@ -0,0 +1,12 @@
+export type TabloImage = {
+  image_id: number,
+  has_title: boolean
+};
+
+export type ShowCounts = {
+  airing_count: number,
+  unwatched_count: number,
+  protected_count: number,
+  watched_and_protected_count: number,
+  failed_count: number
+};
