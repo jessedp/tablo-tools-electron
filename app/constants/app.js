da00cdad9207e8a7f1831d08d8c69320e4728a09
jesse <jessedp@gmail.com>
2020-05-30 10:36:58 -0400

    rearrange, move export in

--- app/constants/app.js
+++ app/constants/app.js
@@ -1,10 +1,16 @@
+export const ON = 1;
+export const OFF = 1;
+
 export const SERIES = 'episode';
 export const MOVIE = 'movie';
 export const EVENT = 'event';
 export const PROGRAM = 'program';
 
-export const ON = 1;
-export const OFF = 1;
+export const EXP_WAITING = 1;
+export const EXP_WORKING = 2;
+export const EXP_DONE = 3;
+export const EXP_CANCEL = 4;
+export const EXP_FAIL = 5;
 
 export const EMPTY_SEARCHALERT = {
   type: '',
@@ -12,8 +18,9 @@ export const EMPTY_SEARCHALERT = {
   matches: []
 };
 
-export const EXP_WAITING = 1;
-export const EXP_WORKING = 2;
-export const EXP_DONE = 3;
-export const EXP_CANCEL = 4;
-export const EXP_FAIL = 5;
+export type ProgramData = {
+  airing: Airing,
+  airings: Array<Airing>,
+  count: number,
+  unwatched: number
+};
