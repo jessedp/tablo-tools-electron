2b427456695de906121d0bea5271644dff9c2547
jesse <jessedp@gmail.com>
2020-06-05 16:04:48 -0400

    add exportList redux stuff

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -3,12 +3,14 @@ import { combineReducers } from 'redux';
 import { connectRouter } from 'connected-react-router';
 import type { HashHistory } from 'history';
 import manageActionList from './actionList';
+import manageExportList from './exportList';
 import { changeView, sendResults } from './search';
 
 export default function createRootReducer(history: HashHistory) {
   return combineReducers<{}, *>({
     router: connectRouter(history),
     actionList: manageActionList,
+    exportList: manageExportList,
     view: changeView,
     results: sendResults
   });
