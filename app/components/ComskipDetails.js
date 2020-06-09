9b76206b6652834344ccb38f83082332e364dcf6
jesse <jessedp@gmail.com>
2020-06-08 21:42:53 -0400

    fixes TABLO-TOOLS-ELECTRON-1Q

--- app/components/ComskipDetails.js
+++ app/components/ComskipDetails.js
@@ -41,15 +41,16 @@ export default class ComskipDetails extends Component<Props, State> {
     // const comskip = await RecDb.asyncCount({ 'video_details.comskip': { $exists: true } });
     const recs = await global.RecDb.asyncFind({});
 
-    const skipStats = { ready: 0, none: 0, error: 0 };
+    const skipStats = { ready: 0, none: 0, error: 0, unk: 0 };
     const skipErrors = {};
 
     const shows = {};
     recs.forEach(rec => {
       const cs = rec.video_details.comskip;
       const title = rec.airing_details.show_title;
-
-      skipStats[cs.state] = skipStats[cs.state] ? skipStats[cs.state] + 1 : 1;
+      let { state } = cs;
+      if (!state) state = 'unk';
+      skipStats[state] = skipStats[state] ? skipStats[state] + 1 : 1;
       if (cs.state === 'ready')
         shows[title] = shows[title] ? shows[title] + 1 : 1;
 
