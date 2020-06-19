dc25a0b156e0b5c5e25e30aa586fa992d97cef36
jesse <jessedp@gmail.com>
2020-06-18 22:31:40 -0400

    fix #30 some more?

--- app/components/LiveTvPage.js
+++ app/components/LiveTvPage.js
@@ -88,7 +88,7 @@ class LiveTvPage extends Component<Props, State> {
                       <td>
                         {rec.channel.major} -{rec.channel.minor}
                         <span className="smaller pl-2">
-                          ({rec.channel.call_sign})
+                          ({rec.channel.callSign})
                         </span>
                       </td>
                       <td>{rec.channel.network}</td>
