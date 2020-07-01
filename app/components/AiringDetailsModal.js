31327e44120296db69b0f45ef2451b3fbf6e1496
jesse <jessedp@gmail.com>
2020-07-01 13:54:42 -0400

    click path -> open in browser

--- app/components/AiringDetailsModal.js
+++ app/components/AiringDetailsModal.js
@@ -1,5 +1,7 @@
 // @flow
+import { shell } from 'electron';
 import React, { useState } from 'react';
+
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 import { Alert } from 'react-bootstrap';
@@ -22,13 +24,29 @@ export default function AiringDetailsModal(props: Props) {
         variant="outline-secondary"
         onClick={() => setShow(true)}
         size="xs"
-        title="info"
+        title="Info"
         className="ml-2"
       >
         <span className="fa fa-info-circle" />
       </Button>
     );
   }
+
+  const selectJson = (node: Object) => {
+    // const { updateValue } = this.props;
+    // const { position } = this.state;
+    // const { workingValue } = this.state;
+
+    if (node.name.includes('path')) {
+      console.log(node);
+      const host = global.Api.device.private_ip;
+      const url = `http://${host}:8885/${node.value}`;
+      shell.openExternal(url);
+    }
+  };
+
+  // eslint-disable-next-line no-underscore-dangle
+  delete airing.data._id;
 
   return (
     <Modal show={show} scrollable onHide={() => setShow(false)} size="lg">
@@ -45,11 +63,19 @@ export default function AiringDetailsModal(props: Props) {
           withActions={OFF}
         />
         <RecordingOverview airing={airing} />
+        <Alert size="sm" variant="dark" className="p-1 pl-2">
+          JSON data
+          <span className="pl-2 smaller">
+            (click any <i>path</i> to open it in the browser)
+          </span>
+        </Alert>
+
         <ReactJson
           src={airing.data}
           enableClipboard
-          collapsed={1}
+          collapsed={0}
           displayDataTypes
+          onSelect={selectJson}
         />
       </Modal.Body>
       <Modal.Footer>
