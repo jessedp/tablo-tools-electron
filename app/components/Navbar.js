3b855c1d14b8022cc2f2c094a74ef900ebd00f62
jesse <jessedp@gmail.com>
2020-07-01 23:56:37 -0400

    split out versionstatus, common update/notification better #46

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,58 +1,27 @@
 // @flow
-import { ipcRenderer, shell } from 'electron';
-import React, { Component, useState } from 'react';
+import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import { LinkContainer } from 'react-router-bootstrap';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Modal from 'react-bootstrap/Modal';
-
-import ReactMarkdown from 'react-markdown';
-
-import { format } from 'date-fns';
-import axios from 'axios';
 
 import { Dropdown, DropdownButton } from 'react-bootstrap';
 import routes from '../constants/routes.json';
 
 import PingStatus from './PingStatus';
 import DbStatus from './DbStatus';
-import RelativeDate from './RelativeDate';
-import getConfig from '../utils/config';
+
 import SelectedBox from './SelectedBox';
 import LogoBox from './Logo';
 import ScreenControls from './ScreenControls';
+import VersionStatus from './VersionStatus';
 
 const { remote } = require('electron');
 
-const { app } = remote;
-
-type File = {
-  url: string,
-  sha512: string,
-  size: number,
-  blockMapSize: number
-};
-
-type UpdateMessage = {
-  available: boolean,
-  info: {
-    version: string,
-    files: Array<File>,
-    path: string,
-    sha512: string,
-    releaseDate: string,
-    releaseName: string,
-    releaseNotes: string
-  }
-};
-
 type Props = { location: Object };
 type State = {
-  updateAvailable: boolean,
-  updateData?: UpdateMessage,
   showToggle: boolean
 };
 
@@ -66,47 +35,12 @@ class Navbar extends Component<Props, State> {
   constructor() {
     super();
     this.state = {
-      updateAvailable: false,
       showToggle: false
     };
     this.lastMousePos = { x: 0, y: 0 };
     this.autohideTimer = null;
 
     (this: any).mouseMove = this.mouseMove.bind(this);
-  }
-
-  async componentDidMount(): * {
-    const checkUpdate = async () => {
-      // electron-updater in main proc for full releases
-      ipcRenderer.send('update-request');
-
-      // pre-release check
-      if (getConfig().notifyBeta) {
-        let msg: UpdateMessage = {};
-        try {
-          const resp = await axios.get(
-            'https://api.github.com/repos/jessedp/tablo-tools-electron/releases'
-          );
-          const data = resp.data[0];
-          if (data.prerelease && `v.${app.getVersion()}` !== data.tag_name) {
-            msg = releaseToUpdateMsg(data);
-          }
-        } catch (e) {
-          console.warn('Problem loading releases from GH:', e);
-        }
-        if (msg) this.processUpdate(msg);
-      }
-    };
-
-    if (process.env.NODE_ENV === 'production') {
-      setInterval(checkUpdate, 1000 * 60 * 60);
-      setTimeout(checkUpdate, 1000);
-    }
-
-    ipcRenderer.on('update-reply', (event, msg) => {
-      this.processUpdate(msg);
-    });
-    // this.processUpdate(versionUpdateTestMessage);
   }
 
   mouseMove = (e: any) => {
@@ -129,25 +63,8 @@ class Navbar extends Component<Props, State> {
     return this.lastMousePos.y - bounds.y < limit;
   };
 
-  async processUpdate(msg: Object) {
-    if (msg.error) {
-      console.error('Problem updating: ', msg.error);
-      this.setState({
-        updateAvailable: false
-      });
-    }
-
-    if (msg.available === true) {
-      await this.setState({ updateAvailable: true, updateData: msg });
-    }
-  }
-
   render() {
-    const { updateAvailable, showToggle } = this.state;
-
-    let { updateData } = this.state;
-
-    if (updateData) updateData = updateData.info;
+    const { showToggle } = this.state;
 
     // dropdown state
     let ddText = 'Browse Recordings';
@@ -261,10 +178,7 @@ class Navbar extends Component<Props, State> {
             <Col md="10" className="smaller pt-1  align-items menu-buttons">
               <div className="d-flex flex-row-reverse">
                 <div>
-                  <VersionStatus
-                    updateData={updateData}
-                    available={updateAvailable}
-                  />
+                  <VersionStatus />
                 </div>
                 <div className="pr-1">
                   <LinkContainer
@@ -289,111 +203,6 @@ class Navbar extends Component<Props, State> {
       </Row>
     );
   }
-}
-
-function VersionStatus(prop) {
-  const [show, setShow] = useState(false);
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-
-  const { updateData, available } = prop;
-
-  if (!updateData || !available) return <></>; //
-
-  let color = 'text-warning ';
-  let type = 'NEW Release ';
-  let isRelease = true;
-  if (updateData.version.match(/[a-zA-Z]/)) {
-    type = 'Pre-release ';
-    color = 'text-secondary';
-    isRelease = false;
-  }
-
-  if (!isRelease && !getConfig().notifyBeta) return <></>; //
-
-  const releaseDate = format(
-    Date.parse(updateData.releaseDate),
-    'ccc M/d/yy @ h:m:s a'
-  );
-  const title = `${updateData.version} available as of ${releaseDate}`;
-  let downloadUrl = `https://github.com/jessedp/tablo-tools-electron/releases/download/v${updateData.version}/${updateData.path}`;
-  if (!updateData.path)
-    downloadUrl = `https://github.com/jessedp/tablo-tools-electron/releases/tag/v${updateData.version}/${updateData.path}`;
-
-  return (
-    <>
-      <div className="pt-1">
-        <Button
-          as="div"
-          className=""
-          variant="outline-light"
-          onClick={handleShow}
-          onKeyDown={handleShow}
-          role="button"
-          title={title}
-          size="xs"
-        >
-          <span className={color} style={{ fontSize: '14px' }}>
-            <span className="fa fa-exclamation-circle" />
-          </span>
-        </Button>
-      </div>
-
-      <Modal show={show} onHide={handleClose} scrollable>
-        <Modal.Header closeButton>
-          <Modal.Title>Update Available!</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <div>
-            {type} <b>{updateData.version}</b> was released{' '}
-            <RelativeDate date={updateData.releaseDate} />.
-          </div>
-          {isRelease ? (
-            ''
-          ) : (
-            <div className="text-danger">
-              This is a pre-release. It may be broken. It may be to test a fix.
-              It may not do anything interesting. You&apos;ve been warned.
-            </div>
-          )}
-          <br />
-          <h6>Notes:</h6>
-          <ReactMarkdown source={updateData.releaseNotes} />
-          <Button
-            className="pt-2 ml-2 mt-3 bolder"
-            variant="success"
-            onClick={() => shell.openExternal(downloadUrl)}
-          >
-            <span className="fa fa-download pr-2" />
-            Download {updateData.version} now!
-          </Button>
-          <div className="pt-2 smaller">
-            Once that&apos;s complete, install it and you&apos;re ready to go!
-          </div>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </> //
-  );
-}
-
-function releaseToUpdateMsg(data): UpdateMessage {
-  return {
-    available: true,
-    info: {
-      version: data.tag_name,
-      files: [],
-      path: '',
-      sha512: '',
-      releaseDate: data.published_at,
-      releaseName: data.name,
-      releaseNotes: data.body
-    }
-  };
 }
 
 export default withRouter(Navbar);
