ad0a2220b873b42cf7fc9f13f9c7a8c39e81aca2
jesse <jessedp@gmail.com>
2021-05-20 13:02:27 -0400

    debounce/throttle functions that did NOT fix ?-fullscreen event madness

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -408,3 +408,53 @@ export function findFfmpegPath(debug = false, log?: any) {
   if (debug && log) log.info(`ffmpegPathReal : ${ffmpegPathReal}`);
   return ffmpegPathReal;
 }
+
+export const debounce = <T extends (...args: any[]) => any>(
+  callback: T,
+  waitFor: number
+) => {
+  let timeout: NodeJS.Timeout | undefined;
+  return (...args: Parameters<T>): ReturnType<T> => {
+    let result: any;
+    if (timeout) clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      result = callback(...args);
+    }, waitFor);
+    return result;
+  };
+};
+
+export const throttle = <F extends (...args: any[]) => any>(
+  func: F,
+  waitFor: number
+) => {
+  const now = () => new Date().getTime();
+  const resetStartTime = () => (startTime = now());
+  let timeout: number;
+  let startTime: number = now() - waitFor;
+  // console.log('throttle', now, startTime);
+  return (...args: Parameters<F>): Promise<ReturnType<F>> =>
+    new Promise((resolve) => {
+      const timeLeft = startTime + waitFor - now();
+      if (timeout) {
+        clearTimeout(timeout);
+      }
+      console.log('throttle - timeLeft', timeLeft);
+      console.log(
+        'startTime + waitFor <= now()',
+        startTime,
+        waitFor,
+        startTime + waitFor,
+        now()
+      );
+      if (startTime + waitFor <= now()) {
+        resetStartTime();
+        resolve(func(...args));
+      } else {
+        timeout = setTimeout(() => {
+          resetStartTime();
+          resolve(func(...args));
+        }, timeLeft);
+      }
+    });
+};
