d00e6729ccf1e74a831ea321be8dfbf2ffaa1c94
jesse <jessedp@gmail.com>
2022-09-01 21:03:08 -0400

    cleanup

--- src/main/utils/db.ts
+++ src/main/utils/db.ts
@@ -1,102 +1,104 @@
 // import Store from 'electron-store';
 import PubSub from 'pubsub-js';
-import { formatDistanceToNow } from 'date-fns';
 import path from 'path';
 import AsyncNedb from 'nedb-async';
 import Store from 'electron-store';
-import { hasDevice } from './Tablo';
+import Debug from 'debug';
+import { hasDevice } from './utils';
 import { getPath } from './config';
 
-const store = new Store();
-// const { AsyncNedb } = window.require('nedb-async');
-// const { AsyncNedb } = window.electron;
+const debug = Debug('tt:db');
 
-// const Store = window.require('electron-store');
+const store = new Store();
 
 const dataDir = getPath('userData');
 
-// const { store } = window.electron;
-// TODO: check/delete old recordings/shows dbs - have to get at IndexDB
 export async function recDbStats() {
   return global.RecDb.asyncCount({});
 }
-export function recDbCreatedDisp() {
-  const last = recDbCreated();
-  let lastBuild = 'never';
 
-  if (last) {
-    lastBuild = formatDistanceToNow(Date.parse(last));
-    lastBuild += ' ago';
-  }
-
-  return lastBuild;
-}
 export const makeRecDb = () => {
   const device: any = store.get('CurrentDevice');
   if (!device.serverid) return null;
   const recDbName = `${device.serverid}-recordings.db`;
   const recFile = path.join(dataDir, recDbName);
+  debug('creating %s at %s', recDbName, recFile);
   return new AsyncNedb({
     filename: recFile,
     autoload: true,
     inMemoryOnly: false,
   });
 };
+
 export const makeShowDb = () => {
   const device: any = store.get('CurrentDevice');
   if (!device.serverid) return null;
   const showDbName = `${device.serverid}-show.db`;
   const showFile = path.join(dataDir, showDbName);
+  debug('creating %s at %s', showDbName, showFile);
   return new AsyncNedb({
     filename: showFile,
     autoload: true,
     inMemoryOnly: false,
   });
 };
+
 export const makeSearchDb = () => {
-  const showDbName = `saved-search.db`;
-  const showFile = path.join(dataDir, showDbName);
+  const searchDbName = `saved-search.db`;
+  const searchFile = path.join(dataDir, searchDbName);
+  debug('creating %s at %s', searchDbName, searchFile);
   return new AsyncNedb({
-    filename: showFile,
+    filename: searchFile,
     autoload: true,
     inMemoryOnly: false,
   });
 };
+
 export const makeChannelDb = () => {
   const device: any = store.get('CurrentDevice');
   if (!device.serverid) return null;
   const channelDbName = `${device.serverid}-channel.db`;
   const channelFile = path.join(dataDir, channelDbName);
+  debug('creating %s at %s', channelDbName, channelFile);
   return new AsyncNedb({
     filename: channelFile,
     autoload: true,
     inMemoryOnly: false,
   });
 };
+
 export const makeNamingDb = () => {
   const device: any = store.get('CurrentDevice');
   if (!device.serverid) return null;
   const namingDbName = `template-naming.db`;
   const namingFile = path.join(dataDir, namingDbName);
+  debug('creating %s at %s', namingDbName, namingFile);
   return new AsyncNedb({
     filename: namingFile,
     autoload: true,
     inMemoryOnly: false,
   });
 };
+
 export const makeExportLoggingDb = () => {
   const device: any = store.get('CurrentDevice');
   if (!device.serverid) return null;
-  const namingDbName = `export-log.db`;
-  const namingFile = path.join(dataDir, namingDbName);
+  const exportDbName = `export-log.db`;
+  const exportFile = path.join(dataDir, exportDbName);
+  debug('creating %s at %s', exportDbName, exportFile);
   return new AsyncNedb({
-    filename: namingFile,
+    filename: exportFile,
     autoload: true,
     inMemoryOnly: false,
   });
 };
+
 export const setupDb = async () => {
-  if (!hasDevice()) return;
+  if (!hasDevice()) {
+    debug('No device, skipping setupDb');
+    return;
+  }
+
   global.RecDb = makeRecDb();
   global.ShowDb = makeShowDb();
   global.ChannelDb = makeChannelDb();
@@ -105,5 +107,3 @@ export const setupDb = async () => {
   global.ExportLogDb = makeExportLoggingDb();
   PubSub.publish('DB_CHANGE', true);
 };
-
-export const { RecDb, ShowDb } = global;
