10e1b4202b0664d3f8e49bf29fa2ebcea9b7a4d2
jesse <jessedp@gmail.com>
2022-09-05 18:36:26 -0400

    change labels

--- src/renderer/components/Duration.tsx
+++ src/renderer/components/Duration.tsx
@@ -1,13 +1,11 @@
-import React from 'react';
-
 export default function Duration(prop: { duration: number[] }) {
   const { duration } = prop;
   if (!duration || duration.length === 0) return <></>;
   const parts = [];
   if (duration[0]) parts.push(`${duration[0]} mo `);
-  if (duration[1]) parts.push(`${duration[1]} day `);
+  if (duration[1]) parts.push(`${duration[1]} d `);
   if (duration[2]) parts.push(`${duration[2]} hr `);
   if (duration[3]) parts.push(`${duration[3]} min `);
-  if (duration[4]) parts.push(`${duration[4]} sec `);
+  if (duration[4]) parts.push(`${duration[4]} s `);
   return <>{parts.join(' ')}</>;
 }
